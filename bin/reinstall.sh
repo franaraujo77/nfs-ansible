6e80be6bb1950c04b1dac8caf60be4d45963a680
Francis Araujo <francis.araujo@gmail.com>
2023-12-17 20:40:00 -0300

    use a more generic way to install all docker compose

--- bin/reinstall.sh
+++ bin/reinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+printf "Uninstalling nfs...\n"
+printf "#######################################################\n\n"
+ansible-playbook -i ./inventory/servers/hosts.ini reboot-nfs.yml
+
 printf "Installing nfs...\n"
 printf "#######################################################\n\n"
 ansible-playbook -i ./inventory/servers/hosts.ini nfs.yml
